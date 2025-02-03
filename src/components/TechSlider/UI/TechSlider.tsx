5e107cb625ce5f92c244508cf782031327cb23ce
Grigor Ohanyan <143016419+HaniusG@users.noreply.github.com>
2025-02-03 21:27:49 +0400

    big updated

--- src/components/TechSlider/UI/TechSlider.tsx
+++ src/components/TechSlider/UI/TechSlider.tsx
@@ -1,13 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './TechSlider.module.scss';
 
+// Image imports
+import jsLogo from '@/assets/images/technologies/javascript.svg';
+import tsLogo from '@/assets/images/technologies/typescript.svg';
+import htmlLogo from '@/assets/images/technologies/html.svg';
+import cssLogo from '@/assets/images/technologies/css.svg';
+import sassLogo from '@/assets/images/technologies/sass.svg';
+import reactLogo from '@/assets/images/technologies/react.svg';
+import reduxLogo from '@/assets/images/technologies/redux.svg';
+import pythonLogo from '@/assets/images/technologies/python.svg';
+import cppLogo from '@/assets/images/technologies/cpp.svg';
+import mysqlLogo from '@/assets/images/technologies/mysql.svg';
+import wordpressLogo from '@/assets/images/technologies/wordpress.svg';
+import fastapiLogo from '@/assets/images/technologies/fastapi.svg';
+import dockerLogo from '@/assets/images/technologies/docker.svg';
+
 const TechSlider: React.FC = () => {
   const sliderRef = useRef<HTMLDivElement>(null);
   let position = 0;
 
   useEffect(() => {
     const slider = sliderRef.current;
-    if (!slider) return; // Safety check in case the ref isn't set
+    if (!slider) return;
 
     const sliderItems = Array.from(slider.children) as HTMLElement[];
     const itemWidth = sliderItems[0].offsetWidth + 20; // Item width + gap
@@ -20,7 +35,7 @@ const TechSlider: React.FC = () => {
 
     // Sliding logic
     const slide = () => {
-      position -= 1; // Move 1px per frame
+      position -= 0.15; // Move 1px per frame
       slider.style.transform = `translateX(${position}px)`;
 
       // Reset position when the first batch is out of view
@@ -37,19 +52,19 @@ const TechSlider: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.slider} ref={sliderRef}>
-        <img src='../../../assets/images/technologies/javascript.svg' alt="JavaScript" />
-        <img src="/icons/typescript.svg" alt="TypeScript" />
-        <img src="/icons/html.svg" alt="HTML" />
-        <img src="/icons/css.svg" alt="CSS" />
-        <img src="/icons/sass.svg" alt="SASS" />
-        <img src="/icons/react.svg" alt="React" />
-        <img src="/icons/redux.svg" alt="Redux" />
-        <img src="/icons/python.svg" alt="Python" />
-        <img src="/icons/cpp.svg" alt="C++" />
-        <img src="/icons/mysql.svg" alt="MySQL" />
-        <img src="/icons/wordpress.svg" alt="WordPress" />
-        <img src="/icons/fastapi.svg" alt="FastAPI" />
-        <img src="/icons/docker.svg" alt="Docker" />
+        <img src={jsLogo} alt="JavaScript" />
+        <img src={tsLogo} alt="TypeScript" />
+        <img src={htmlLogo} alt="HTML" />
+        <img src={cssLogo} alt="CSS" />
+        <img src={sassLogo} alt="SASS" />
+        <img src={reactLogo} alt="React" />
+        <img src={reduxLogo} alt="Redux" />
+        <img src={pythonLogo} alt="Python" />
+        <img src={cppLogo} alt="C++" />
+        <img src={mysqlLogo} alt="MySQL" style={{width: "120px", margin: "0 -15px"}}/>
+        <img src={wordpressLogo} alt="WordPress" />
+        <img src={fastapiLogo} alt="FastAPI" />
+        <img src={dockerLogo} alt="Docker" />
       </div>
     </div>
   );
--- src/pages/Home/components/Landing/UI/Landing.tsx
+++ src/pages/Home/components/Landing/UI/Landing.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./Landing.module.scss";
-import ProfileJpg from '../../../../../assets/images/avatar/Profile.jpg';
-import ProfileWebp from '../../../../../assets/images/avatar/Profile.webp'
-import Button from "../../../../../components/Button";
-import GitHub from '../../../../../assets/images/logos/github.svg'
-import TechSlider from "../../../../../components/TechSlider";
+import ProfileJpg from '@/assets/images/avatar/Profile.jpg';
+import ProfileWebp from '@/assets/images/avatar/Profile.webp'
+import Button from "@/components/Button";
+import GitHub from '@/assets/images/logos/github.svg'
+import LinkedIn from '@/assets/images/logos/linkedin.svg'
+import TechSlider from "@/components/TechSlider";
 
 const Landing = () => {
   return (
@@ -24,11 +25,12 @@ const Landing = () => {
         <h3>Frontend React Developer</h3>
       </div>
       <div className={styles.buttonDiv}>
-        <Button color='black'>My CV</Button>
+      <a href='https://drive.google.com/file/d/1-t5rtp4ji2dZ6T6gAfaG_txWBd5EVs3R/view' target="_blank"><Button color='black'>My CV</Button></a>
         <Button color='black' fill>Contact Info</Button>
       </div>
-      <div className=''>
-        <a><img src={GitHub} alt='aaaa'/></a>
+      <div className={styles.contactLinks}>
+        <a href="https://github.com/HaniusG" target="_blank"><img src={GitHub} alt='GitHub Link'/></a>
+        <a href="https://www.linkedin.com/in/grigor-ohanyan-90a910268/" target="_blank"><img src={LinkedIn} alt='LinkedIn Link'/></a>
       </div>
       <TechSlider/>
     </div>
