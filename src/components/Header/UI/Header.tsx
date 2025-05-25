e6d089bbeca15ea9b76221e20319219776e69205
Grigor Ohanyan <143016419+HaniusG@users.noreply.github.com>
2025-05-25 16:13:25 +0400

    many changes

--- src/components/Header/UI/Header.tsx
+++ src/components/Header/UI/Header.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 
 const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
   return (
     <header className={styles.headerWrapper}>
       <div className={styles.header}>
-        <div className={styles.logo}>
-          <a href="#">
-            <img src='/assets/images/logos/godev.svg' className={styles.logoIcon} alt="Logo" />
-          </a>
-        </div>
-        <nav className={styles.nav}>
+        {!menuOpen && (
+          <div className={styles.logo}>
+            <a href="#">
+              <img src='/assets/images/logos/godev.svg' className={styles.logoIcon} alt="Logo" />
+            </a>
+          </div>
+        )}
+        <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ''}`}>
           <a href="#projects" className={styles.link}>Projects</a>
           <a href="#experience" className={styles.link}>Experience</a>
           <a href="#contact" className={styles.link}>Contact Me</a>
         </nav>
+        <div className={styles.mobileMenu} onClick={toggleMenu}>
+          {menuOpen ? '✖' : '☰'}
+        </div>
       </div>
     </header>
   );
--- src/pages/Home/components/Landing/UI/Landing.tsx
+++ src/pages/Home/components/Landing/UI/Landing.tsx
@@ -20,14 +20,14 @@ const Landing = () => {
         <h3>Frontend React Developer</h3>
       </div>
       <div className={styles.buttonDiv}>
-      <a href='https://drive.google.com/file/d/1vEGbN-rPJBizw25gh3aOIikmUinHmwWP/view?usp=sharing' target="_blank"><Button color='black'>My CV</Button></a>
-        <Button color='black' fill>Contact Info</Button>
+        <a href='https://drive.google.com/file/d/1vEGbN-rPJBizw25gh3aOIikmUinHmwWP/view?usp=sharing' target="_blank"><Button color='black'>My CV</Button></a>
+        <a href='#contact'><Button color='black' fill>Contact Info</Button></a>
       </div>
       <div className={styles.contactLinks}>
-        <a href="https://github.com/HaniusG" target="_blank"><img src='/assets/images/logos/github.svg' alt='GitHub Link'/></a>
-        <a href="https://www.linkedin.com/in/grigor-ohanyan-90a910268/" target="_blank"><img src='/assets/images/logos/linkedin.svg' alt='LinkedIn Link'/></a>
+        <a href="https://github.com/HaniusG" target="_blank"><img src='/assets/images/logos/github.svg' alt='GitHub Link' /></a>
+        <a href="https://www.linkedin.com/in/grigor-ohanyan-90a910268/" target="_blank"><img src='/assets/images/logos/linkedin.svg' alt='LinkedIn Link' /></a>
       </div>
-      <TechSlider/>
+      <TechSlider />
     </div>
   );
 };
