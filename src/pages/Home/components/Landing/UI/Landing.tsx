21ff87de2c0e4ef7ca90b081106da61e32c5f58e
Grigor Ohanyan <143016419+HaniusG@users.noreply.github.com>
2025-09-22 19:27:51 +0400

    new fix

--- src/pages/Home/components/Landing/UI/Landing.tsx
+++ src/pages/Home/components/Landing/UI/Landing.tsx
@@ -35,7 +35,7 @@ const Landing = () => {
       </div>
       <p>Hi, I am</p>
       <h2>Grigor Ohanyan</h2>
-      <h3>Frontend React Developer</h3>
+      <h3>Frontend Developer</h3>
       <div className={styles.buttonDiv}>
         <a href='https://drive.google.com/file/d/1QgUBaSaFNXHxSEV2BxfNHFm0ncLPXQmv/view?usp=drive_link' target="_blank"><Button color='rgb(41 102 255)'>My CV</Button></a>
         <a href='#contact'><Button color='rgb(41 102 255)' fill>Contact Info</Button></a>
